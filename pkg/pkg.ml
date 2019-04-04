5dcdf1b2778106e591acf253dd26f29065d32aaa
Gabe Levi <glevi@fb.com>
2019-04-04 09:29:05 -0700

    Switch to dune

--- pkg/pkg.ml
+++
@@ -1,6 +0,0 @@
-(* Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved *)
-
-#use "topfind"
-#require "topkg-jbuilder"
-
-let () = Topkg_jbuilder.describe ~licenses:[ Topkg.Pkg.std_file "LICENSE" ] ()
