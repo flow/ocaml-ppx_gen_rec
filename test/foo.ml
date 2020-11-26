71d1d12085acc4f969b09db23926df1b7582be40
Sacha Ayoun <sachaayoun@gmail.com>
2020-11-26 13:27:00 +0000

    tests with ppx_deriving

--- test/foo.ml
+++ test/foo.ml
@@ -1,12 +1,18 @@
 
 module%gen rec Foo : sig
-  type t = string
+  type t = string [@@deriving show]
 end = Foo
 
-let _ : Foo.t = "foo"
+let foo : Foo.t = "foo"
 
-let _ : ('a, 'b) Flow_ast.Program.t' = {
-  statements =[];
-  all_comments=[];
-  comments=None
-}
+let () = print_endline (Foo.show foo)
+
+
+let () =
+  let empty_program = Flow_ast.Program.{
+    statements = [];
+    comments = None;
+    all_comments = [];
+  } in
+  let nop = fun _ _ -> () in 
+  print_endline (Flow_ast.Program.show nop nop (None, empty_program))
